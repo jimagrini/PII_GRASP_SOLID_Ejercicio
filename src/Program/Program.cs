d261813df98b4367bbd6a462d327144fa2529f1e
jimagrini <juan.magrini@correo.ucu.edu.uy>
2022-09-04 22:11:28 -0300

    actualizando solucion

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -26,8 +26,7 @@ namespace Full_GRASP_And_SOLID
             recipe.AddStep(new Step(GetProduct("Café"), 100, GetEquipment("Cafetera"), 120));
             recipe.AddStep(new Step(GetProduct("Leche"), 200, GetEquipment("Hervidor"), 60));
             recipe.GetProductionCost();
-            ConsolePrinter Bill = new ConsolePrinter(recipe);
-            Console.WriteLine(Bill);
+            ConsolePrinter recipeBody = new ConsolePrinter(recipe);
         }
 
         private static void PopulateCatalogs()
